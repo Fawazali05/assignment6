9527d73a386204399aec5915c52d14657154324d
Fawazali05 <fawazali.fas@gmail.com>
2025-07-06 17:12:01 +0530

    some changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const App = () => {
-  return <h1>Hello from Fawaz Ali. I am using docker compose!</h1>;
-};
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
